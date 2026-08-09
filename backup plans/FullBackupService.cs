b1d8c49e839288b2e53acccc1dcf450e2e6fb4e3
Antek <antekch06@gmail.com>
2026-08-09 13:17:13 +0200

    Add backup services

---
+++ backup plans/FullBackupService.cs
@@ -0,0 +1,29 @@
+﻿
+using BackupSystem.Enums;
+
+namespace BackupSystem
+{
+    public  class FullBackupService : IBackupStrategy
+    {
+       
+        public BackupMetrics ExecuteBackup(ConnectionParameters _connectionParameters, EnumDataBaseType _databaseType, EnumBackupPlans _backupType, string backupPath)
+        {
+
+            switch (_databaseType)
+            {
+                case EnumDataBaseType.MariaDB:
+                    BackupMetrics maria = FullMaria.FullMariaBackup(_connectionParameters, _databaseType, _backupType, backupPath);
+                    return maria;
+                case EnumDataBaseType.PostgreSQL:
+                    break;
+                case EnumDataBaseType.MySQL:
+                    break;
+                default:
+                    throw new NotImplementedException($"Backup strategy for {_databaseType} is not implemented.");
+            }
+            return null;
+        }
+
+    }
+}
+
